09f670d3beec8d8cb1662c7bdaa88b5378c7cc3f
Josh Eklund <eklun124@umn.edu>
2022-02-07 12:12:31 -0600

    Complete Lab 3
    This commit completes the task outlined in the Unity Lab 3 write up.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -9,12 +9,15 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Button creditsOpenButton;
     [SerializeField] private Button creditsCloseButton;
     [SerializeField] private GameObject creditsUI;
+    [SerializeField] private Button notQuitePlatformerButton;
     // Start is called before the first frame update
     void Start()
     {
         startButton.onClick.AddListener(() => LoadingScreen.LoadScene("GunScene"));
         creditsOpenButton.onClick.AddListener(() => creditsUI.SetActive(true));
         creditsCloseButton.onClick.AddListener(()=> creditsUI.SetActive(false));
+        notQuitePlatformerButton.onClick.AddListener(() => LoadingScreen.LoadScene("NotQuitePlatformer"));
+
 
         
     }
